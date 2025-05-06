d15053b7bef5e5f25ee10f24ee1bcfb4d93b47cc
niuniumart <1735716945@qq.com>
2025-05-06 09:44:20 +0800

    up log

--- middlewares/nlog/gormlog.go
+++ middlewares/nlog/gormlog.go
@@ -1,11 +1,12 @@
-package log
+package nlog
 
 import (
 	"context"
+	"time"
+
 	"github.com/go-kratos/kratos/v2/errors"
 	"gorm.io/gorm"
 	log2 "gorm.io/gorm/logger"
-	"time"
 )
 
 type GormLogger struct {
@@ -14,10 +15,6 @@ type GormLogger struct {
 
 // NewGormLogger 传参单位为ms
 func NewGormLogger(slowThresholdMillisecond int64) *GormLogger {
-	if GetDefaultLogger() == nil {
-		panic("请初始化log包下的logger！！！")
-	}
-
 	return &GormLogger{
 		slowThreshold: time.Duration(slowThresholdMillisecond) * time.Millisecond,
 	}
@@ -28,15 +25,15 @@ func (l *GormLogger) LogMode(level log2.LogLevel) log2.Interface {
 }
 
 func (l *GormLogger) Info(ctx context.Context, s string, i ...interface{}) {
-	InfoContextf(ctx, s, i...)
+	Infof(ctx, s, i...)
 }
 
 func (l *GormLogger) Warn(ctx context.Context, s string, i ...interface{}) {
-	WarnContextf(ctx, s, i...)
+	Warnf(ctx, s, i...)
 }
 
 func (l *GormLogger) Error(ctx context.Context, s string, i ...interface{}) {
-	ErrorContextf(ctx, s, i...)
+	Errorf(ctx, s, i...)
 }
 
 func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
@@ -50,20 +47,20 @@ func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (stri
 	if err != nil {
 		// 记录未找到的错误使用 warning 等级
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			InfoContextf(ctx, "Database ErrRecordNotFound，sql: %s, time: %s, rows: %d", sql, executeTime.String(), rows)
+			Infof(ctx, "Database ErrRecordNotFound，sql: %s, time: %s, rows: %d", sql, executeTime.String(), rows)
 		} else {
 			// 其他错误使用 error 等级
-			ErrorContextf(ctx, "Database Error sql: %s, time: %s, rows: %d, err: %v", sql, executeTime.String(), rows, err)
+			Errorf(ctx, "Database Error sql: %s, time: %s, rows: %d, err: %v", sql, executeTime.String(), rows, err)
 		}
 		return
 	}
 
 	// 慢查询日志
 	if l.slowThreshold != 0 && executeTime > l.slowThreshold {
-		InfoContextf(ctx, "Database Slow Log sql: %s, time: %s, rows: %d", sql, executeTime.String(), rows)
+		Infof(ctx, "Database Slow Log sql: %s, time: %s, rows: %d", sql, executeTime.String(), rows)
 	} else {
 		// 记录所有 SQL 请求
-		InfoContextf(ctx, "Database Query: %s, time: %s, rows: %d, err: %v", sql, executeTime.String(), rows, err)
+		Infof(ctx, "Database Query: %s, time: %s, rows: %d, err: %v", sql, executeTime.String(), rows, err)
 
 	}
 
